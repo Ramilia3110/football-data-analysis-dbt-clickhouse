6f5ed8b8b24cacb2eddd19faa5e80ffde131d695
Ramilia Imankulova <ramilia3110@gmail.com>
2025-10-19 21:23:26 +0600

    edit query

--- dbt_project/football_dbt/models/analysis/2_rep_joined_after_count.sql
+++ dbt_project/football_dbt/models/analysis/2_rep_joined_after_count.sql
@@ -3,16 +3,15 @@
 SELECT
     t1.name,
     t1.club,
-    -- Count players (t2) in the same club
-    -- whose joined_club date is strictly LATER than t1.joined_club
+ 
     COUNT(t2.player_id) AS players_joined_after
 FROM
-    {{ ref('clean_players') }} AS t1 -- The player being evaluated ('him')
+    {{ ref('clean_players') }} AS t1 
 LEFT JOIN
-    {{ ref('clean_players') }} AS t2 -- The other players
+    {{ ref('clean_players') }} AS t2 
     ON
-        t1.club = t2.club                  -- Players must be in the same club
-        AND t2.joined_club > t1.joined_club -- t2 joined strictly AFTER t1
+        t1.club = t2.club                 
+        AND t2.joined_club > t1.joined_club 
 GROUP BY
     t1.name,
     t1.club
